a19304d226982add29da59982cc7841ab922512b
rewpt <zpermack@gmail.com>
2021-06-02 17:59:10 +0000

    odd.js exercise working and  completed

---
+++ odd.js
@@ -0,0 +1,11 @@
+const isOdd = (num) => {
+  let val = num % 2;
+  if(val > 0){
+    return true;
+  }else{
+    return false;
+  }; 
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
