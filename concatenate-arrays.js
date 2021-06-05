7ab06b3e5137b9474a1a0553ad66590df98038ab
rewpt <zpermack@gmail.com>
2021-06-05 19:29:24 +0000

    updated comments on concatenate-arrays.js

--- concatenate-arrays.js
+++ concatenate-arrays.js
@@ -3,9 +3,11 @@ const concat = (arr1, arr2) => {
   else if(arr2[0] === undefined) {
     return arr1; }
   else if(arr1[0] === undefined) { return arr2; }
-
+  // merge the array together placing a comma in between so the end of 1 and beginning of 2 are separated
   let concatArr = arr1 + ',' + arr2;
+  //split the items at the commas to form an array of the components
   let splitArr = concatArr.split(',');
+  //turn each item into a number from a string
   for(let i = 0; i < splitArr.length; i++) {
     splitArr[i] = Number(splitArr[i]);
   }
