2edbd387fdba4eac4473745b480335f3de08b59b
rewpt <zpermack@gmail.com>
2021-06-07 20:37:15 +0000

    just commiting my udacity workspace, just a bunch of random stuff

--- udacity.js
+++ udacity.js
@@ -10,7 +10,7 @@
  */
 
 
-// creates a line of * for a given length
+/* creates a line of * for a given length
 function makeLine(length) {
   var line = "";
   for (var j = 1; j <= length; j++) {
@@ -19,13 +19,38 @@ function makeLine(length) {
   return line + "\n";
 }
 
-// your code goes here.  Make sure you call makeLine() in your own code.
+your code goes here.  Make sure you call makeLine() in your own code.
 const buildTriangle = (x)  => {
   let fullTri = ''
   for(let i = 0; i < x; i++){
     fullTri += makeLine(i);
-  }  
+  }
   return fullTri;
 }
-console.log(buildTriangle(6));
+console.log(buildTriangle(6)); */
 // test your code by uncommenting the following line
+
+const merge = (arr1, arr2) => {
+  if(arr1[0] === undefined && arr2[0] === undefined) { return [] }
+  else if(arr2[0] === undefined) {
+    return arr1; }
+  else if(arr1[0] === undefined) { return arr2; }
+  //set up an array to return
+  let mergedArr = [];
+  console.log('length1: ' + arr1.length)
+  console.log('length2: ' + arr2.length)
+  const iLength = arr1.length;
+  const jLength = arr2.length;
+  for(let i = 0; i < iLength; i++){
+    for(let j = 0; j < jLength; j++){
+      //if the number is arr2 is smaller or equal in size it can go behind that number
+      if(arr2[j] <= arr1[i]){
+        mergedArr.push(arr2[j]);
+      }
+      console.log(mergedArr)
+    }
+  }
+  return mergedArr;
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
